94f8c314530ed96776f3ae54c1878d3c7b7471fc
Ivan Silva <ivan.elisandro@gmail.com>
2026-01-04 17:19:00 -0300

    stage 2: add input for username and greeting
    important files:
    - ./Chatbot with Python/task/bot/bot.py
    - ./Chatbot with Python/task/bot/stage2.md

--- Chatbot with Python/task/bot/bot.py
+++ Chatbot with Python/task/bot/bot.py
@@ -1,5 +1,23 @@
-# write your code here
-bot_name = "Astro"
-birth_year = 2026
-print(f"Hello! My name is {bot_name}.")
-print(f"I was created in {birth_year}.")
+class Astro:
+    def __init__(self):
+        self.name = "Astro"
+        self.birth_year = 2026
+        self.username = ""
+
+    def run(self):
+        print(f"Hello! My name is {self.name}.")
+        print(f"I was created in {self.birth_year}.")
+        self.remind_name()
+        self.greet()
+
+    def remind_name(self):
+        print("Please, remind me your name.")
+        self.username = input()
+
+    def greet(self):
+        print(f"What a great name you have, {self.username}!")
+
+
+if __name__ == "__main__":
+    astro = Astro()
+    astro.run()
---
+++ Chatbot with Python/task/bot/stage2.md
@@ -0,0 +1,20 @@
+# Stage 1:
+
+In this stage we will include a way for us to inform our name.
+
+## Objectives
+
+The bot should:
+
+- Ask for the user to input its name;
+- Print a greeting with the username;
+
+## Example
+
+```bash
+Hello! My name is Aid.
+I was created in 2025.
+Please, remind me your name.
+> Max
+What a great name you have, Max!
+```
